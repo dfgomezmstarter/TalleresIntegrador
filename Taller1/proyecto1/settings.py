a76fcbf2d2a5512f7d6cff5e1edc4634176e1266
Daniel Felipe Gómez Martínez <dfgomezm@eafit.edu.co>
2020-05-12 23:47:10 -0500

    Taller 2 terminado

--- Taller1/proyecto1/settings.py
+++ Taller1/proyecto1/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'proyecto1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'taller3',
+        'USER': 'dfgomezm@taller3',
+        'PASSWORD': 'Dodana05',
+        'HOST': 'taller3.mysql.database.azure.com',
+        'PORT': '3306',
     }
 }
 
